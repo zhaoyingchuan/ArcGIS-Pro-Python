b8d4e7699494a2dd985e24a92c2d6cb7e59f44c2
zhaoyingchuan <18146612665@163.com>
2020-01-26 10:56:31 +0800

    修改sample.py

--- sample.py
+++ sample.py
@@ -1,18 +1,5 @@
-import arcpy, os, sys
+import wikipediaapi
+wiki_wiki = wikipediaapi.Wikipedia('zh')
 
-relpath = os.path.dirname(sys.argv[0])
-
-p = arcpy.mp.ArcGISProject(os.path.join(relpath, 'SingleSymbol.aprx'))
-m = p.listMaps('Map')[0]
-l = m.listLayers('Airports*')[0]
-sym = l.symbology
-
-if sym.renderer.type == 'SimpleRenderer':
-    sym.renderer.label = 'Airports'
-    sym.renderer.description = 'Nonprimary and Primary'
-    sym.renderer.symbol.applySymbolFromGallery('Airport')
-    sym.renderer.symbol.size = 12
-
-    l.symbology = sym
-
-p.saveACopy(os.path.join(relpath, 'SavedOutput.aprx'))
+page_py = wiki_wiki.page('中国')
+print(page_py.exists())
