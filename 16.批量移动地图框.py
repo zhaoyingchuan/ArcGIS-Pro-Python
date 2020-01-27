0dab075e68290936fde711d9f6d09cf03306fd88
zhaoyingchuan <18146612665@163.com>
2020-01-27 10:29:44 +0800

    修改完善代码

--- 16.批量移动地图框.py
+++ 16.批量移动地图框.py
@@ -1,11 +1,11 @@
 import arcpy
-aprx = arcpy.mp.ArcGISProject('CURRENT')
+aprx = arcpy.mp.ArcGISProject(r"D:\Desktop\南昌市健康资源分析\南昌市健康资源分析\南昌市健康资源分析.aprx")
 lyt = aprx.listLayouts("固定时段服务区")[0]
 elmlist = lyt.listElements("MAPFRAME_ELEMENT")
 for elm in elmlist:
     print(elm.name)
-    elm.elementPositionX = elm.elementPositionX + 1
-    elm.elementPositionY = elm.elementPositionY + 1
+    elm.elementPositionX = elm.elementPositionX + 10
+    elm.elementPositionY = elm.elementPositionY + 10
     print(elm.elementPositionX)
     print(elm.elementPositionY)
 aprx.save()
