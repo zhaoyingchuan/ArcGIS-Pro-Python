c169a8423054eaa213e748449c09c3e4a9e6b5c6
zhaoyingchuan <18146612665@163.com>
2020-01-28 17:13:40 +0800

    修改完善代码

--- 24.判断某个字段是否存在于要素类属性表中.py
+++ 24.判断某个字段是否存在于要素类属性表中.py
@@ -1,8 +1,10 @@
 import arcpy
 feature_class = r'D:\Desktop\南昌市健康资源分析\固定时段服务区.gdb\公共康体保健\各类球类场所步行15分钟'
 fields = arcpy.ListFields(feature_class)
+uniquelist = []
 for field in fields:
-    print(field.name)
-
-
-
+    uniquelist.append(field.name)
+print(uniquelist)
+fid = 'Text'
+if fid in uniquelist:
+    print('True')
