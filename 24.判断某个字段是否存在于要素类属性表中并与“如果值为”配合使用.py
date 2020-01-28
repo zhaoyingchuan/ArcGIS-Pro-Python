ca2b8503ac385ba9cd0e6b4fa6b0cc5075ccb56c
zhaoyingchuan <18146612665@163.com>
2020-01-28 21:01:32 +0800

    修改完善代码

--- 24.判断某个字段是否存在于要素类属性表中并与“如果值为”配合使用.py
+++ 24.判断某个字段是否存在于要素类属性表中并与“如果值为”配合使用.py
@@ -1,5 +1,6 @@
 import arcpy
-feature_class =  arcpy.GetParameterAsText(0)
+
+feature_class = arcpy.GetParameterAsText(0)
 fields = arcpy.ListFields(feature_class)
 uniquelist = []
 for field in fields:
