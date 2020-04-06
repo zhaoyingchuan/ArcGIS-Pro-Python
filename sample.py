d55ecc8f001ce151307609552c62f6ffb4268fa7
zhaoyingchuan <18146612665@163.com>
2020-04-06 10:37:39 +0800

    修改完善代码

--- sample.py
+++ sample.py
@@ -12,22 +12,3 @@ for i in range(re):
 print(e)
 print(sum(e))
 
-import time
-import arcpy
-aprx = arcpy.mp.ArcGISProject("CURRENT")
-map=aprx.listMaps("地图")[0]
-# Create update cursor for feature class
-rows = arcpy.UpdateCursor("D:\\Documents\\ArcGIS\\Projects\\MyProject\\MyProject.gdb\\南昌市渔网_label")
-# Update the field used in buffer so the distance is based on the
-# road type. Road type is either 1, 2, 3, or 4. Distance is in meters.
-i=0
-for row in rows:
-    # Fields from the table can be dynamically accessed from the
-    # row object. Here fields named BUFFER_DISTANCE and ROAD_TYPE
-    # are used
-    row.setValue("value", e[i])
-    rows.updateRow(row)
-    i=i+1
-# Delete cursor and row objects to remove locks on the data.
-del row
-del rows
