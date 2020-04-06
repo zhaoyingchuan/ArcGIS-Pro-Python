dbeab837d6ac989b8d1be2defe5a23954917df64
zhaoyingchuan <18146612665@163.com>
2020-04-06 10:31:40 +0800

    修改完善代码

--- 函数/1.向表中插入 新行并给对应字段赋值.py
+++ 函数/1.向表中插入 新行并给对应字段赋值.py
@@ -1,14 +1,15 @@
-# coding=gbk
+#coding=utf-8
 import arcpy
 
 # Create insert cursor for table
-rows = arcpy.InsertCursor("c:/base/data.gdb/roads_lut")
+rows = arcpy.InsertCursor("D:\\Documents\\ArcGIS\\Default.gdb\\南昌市渔网_label")
 # Create 25 new rows. Set the initial row ID and distance values
-for x in range(1, 26):
+for x in range(1, 10):
     row = rows.newRow()
-    row.setValue("rowid", x)
-    row.setValue("distance", 100)
+    row.setValue("OID", 500)
+    row.setValue("value", 100)
     rows.insertRow(row)
 # Delete cursor and row objects to remove locks on the data
 del row
 del rows
+
