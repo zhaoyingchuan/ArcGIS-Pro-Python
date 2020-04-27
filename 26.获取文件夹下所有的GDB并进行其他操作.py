84c99fa3e5fe6709740749a77d1ae4c915cdbb8a
zhaoyingchuan <18146612665@163.com>
2020-04-27 23:04:50 +0800

    修改完善代码

--- 26.获取文件夹下所有的GDB并进行其他操作.py
+++ 26.获取文件夹下所有的GDB并进行其他操作.py
@@ -6,15 +6,13 @@ for root, dirs, files in os.walk(file_dir):
     if ".gdb" in root:
         print(root)
 
-        # arcpy.env.workspace = root
-        # for fc in arcpy.ListFeatureClasses():#未指定feature_dataset则表示只列出独立要素类
-        #     arcpy.Delete_management(fc)
-        #     print("  已删除  "+fc)
-        # for fc in arcpy.ListTables():
-        #     arcpy.Delete_management(fc)
-        #     print("  已删除  "+fc)
-
-
+        arcpy.env.workspace = root
+        for fc in arcpy.ListFeatureClasses():#未指定feature_dataset则表示只列出独立要素类
+            arcpy.Delete_management(fc)
+            print("  已删除  "+fc)
+        for fc in arcpy.ListTables():
+            arcpy.Delete_management(fc)
+            print("  已删除  "+fc)
 
         arcpy.env.workspace = root
         datasets = arcpy.ListDatasets("基础数据", "Feature")
