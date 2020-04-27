5cdde16fda7b292a75d817ee1f3b6ff764690f93
zhaoyingchuan <18146612665@163.com>
2020-04-27 22:50:52 +0800

    修改完善代码

--- 08.修改图层名为要素类别名.py
+++ 08.修改图层名为要素类别名.py
@@ -2,13 +2,10 @@
 import arcpy
 
 aprx = arcpy.mp.ArcGISProject('CURRENT')
-lyt = aprx.listLayouts('固定时段服务区')[0]
-mflist = lyt.listElements("MAPFRAME_ELEMENT")
-for mf in mflist:
-    map = mf.map
-    for lyr in map.listLayers():
-        if lyr.isFeatureLayer:
-            text = arcpy.Describe(lyr).aliasName
-            lyr.name = text
+map = aprx.listMaps('地图')[0]
+for lyr in map.listLayers():
+    # if lyr.isFeatureLayer:
+        text = arcpy.Describe(lyr).aliasName
+        lyr.name = text
 aprx.save()
 
