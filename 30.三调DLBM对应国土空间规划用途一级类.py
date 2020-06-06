2fcb4d68a5f2891e68645af1d3d20d863ca10673
zhaoyingchuan <18146612665@163.com>
2020-06-06 17:23:04 +0800

    修改代码

---
+++ 30.三调DLBM对应国土空间规划用途一级类.py
@@ -0,0 +1,99 @@
+def fill(Field):
+    if Field == '0304':
+        return '17 湿地'
+    elif Field == '0306':
+        return '17 湿地'
+    elif Field == '0402':
+        return '17 湿地'
+    elif Field == '0603':
+        return '16 采矿盐田用地'
+    elif Field == '1106':
+        return '17 湿地'
+    elif Field == '1108':
+        return '17 湿地'
+    elif Field == '0101':
+        return '01 耕地'
+    elif Field == '0102':
+        return '01 耕地'
+    elif Field == '0103':
+        return '01 耕地'
+    elif Field == '0201':
+        return '02 园地'
+    elif Field == '0202':
+        return '02 园地'
+    elif Field == '0203':
+        return '02 园地'
+    elif Field == '0204':
+        return '02 园地'
+    elif Field == '0301':
+        return '03 林地'
+    elif Field == '0302':
+        return '03 林地'
+    elif Field == '0305':
+        return '03 林地'
+    elif Field == '0307':
+        return '03 林地'
+    elif Field == '0508':
+        return '09 仓储用地'
+    elif Field == '0601':
+        return '08 工业用地'
+    elif Field == '0602':
+        return '16 采矿盐田用地'
+    elif Field == '0701':
+        return '06 居住用地'
+    elif Field == '0702':
+        return '06 居住用地'
+    elif Field == '08H1':
+        return '07 公共设施用地'
+    elif Field == '0810':
+        return '12 绿地与广场用地'
+    elif Field == '0810A':
+        return '12 绿地与广场用地'
+    elif Field == '09':
+        return '15 特殊用地'
+    elif Field == '1001':
+        return '14 区域基础设施用地'
+    elif Field == '1002':
+        return '10 道路与交通设施用地'
+    elif Field == '1003':
+        return '14 区域基础设施用地'
+    elif Field == '1004':
+        return '10 道路与交通设施用地'
+    elif Field == '1005':
+        return '10 道路与交通设施用地'
+    elif Field == '1006':
+        return '05 其他农用地'
+    elif Field == '1007':
+        return '14 区域基础设施用地'
+    elif Field == '1008':
+        return '14 区域基础设施用地'
+    elif Field == '1009':
+        return '14 区域基础设施用地'
+    elif Field == '1101':
+        return '19 陆地水域'
+    elif Field == '1102':
+        return '19 陆地水域'
+    elif Field == '1103':
+        return '19 陆地水域'
+    elif Field == '1104':
+        return '05 其他农用地'
+    elif Field == '1107':
+        return '05 其他农用地'
+    elif Field == '1109':
+        return '14 区域基础设施用地'
+    elif Field == '1201':
+        return '13 留白用地'
+    elif Field == '1202':
+        return '05 其他农用地'
+    elif Field == '1203':
+        return '05 其他农用地'
+    elif Field == '1204':
+        return '18 其他自然保留地'
+    elif Field == '1205':
+        return '18 其他自然保留地'
+    elif Field == '1206':
+        return '18 其他自然保留地'
+    elif Field == '1207':
+        return '18 其他自然保留地'
+    else:
+        return ""
--- sample.py
+++ sample.py
@@ -1,73 +1,47 @@
-import os
-import arcpy
-
-
-
-outpath="D:\\Desktop"
-outgdbname="汇总数据库"
-
-if arcpy.Exists(outpath+"\\"+outgdbname+".gdb"):
-    pass
-else:
-    arcpy.CreateFileGDB_management(outpath,outgdbname) #建立输出地理数据库
-
-arcpy.env.workspace = "D:\\Desktop\\吉安市吉安县_生态保护红线基础数据要素图层.gdb"
-datasets = arcpy.ListDatasets()
-for ds in datasets:
-    desc = arcpy.Describe(ds)
-    sr = desc.spatialReference
-    if arcpy.Exists(outpath + "\\" + outgdbname + ".gdb"+"\\"+ds):
-        pass
+def fill(Field):
+    if Field == '1001':
+        return '14 区域基础设施用地'
+    elif Field == '1002':
+        return '10 道路与交通设施用地'
+    elif Field == '1003':
+        return '14 区域基础设施用地'
+    elif Field == '1004':
+        return '10 道路与交通设施用地'
+    elif Field == '1005':
+        return '10 道路与交通设施用地'
+    elif Field == '1006':
+        return '05 其他农用地'
+    elif Field == '1007':
+        return '14 区域基础设施用地'
+    elif Field == '1008':
+        return '14 区域基础设施用地'
+    elif Field == '1009':
+        return '14 区域基础设施用地'
+    elif Field == '1101':
+        return '19 陆地水域'
+    elif Field == '1102':
+        return '19 陆地水域'
+    elif Field == '1103':
+        return '19 陆地水域'
+    elif Field == '1104':
+        return '05 其他农用地'
+    elif Field == '1107':
+        return '05 其他农用地'
+    elif Field == '1109':
+        return '14 区域基础设施用地'
+    elif Field == '1201':
+        return '13 留白用地'
+    elif Field == '1202':
+        return '05 其他农用地'
+    elif Field == '1203':
+        return '05 其他农用地'
+    elif Field == '1204':
+        return '18 其他自然保留地'
+    elif Field == '1205':
+        return '18 其他自然保留地'
+    elif Field == '1206':
+        return '18 其他自然保留地'
+    elif Field == '1207':
+        return '18 其他自然保留地'
     else:
-        arcpy.CreateFeatureDataset_management(outpath+"\\"+outgdbname+".gdb",ds,sr)  # 建立输出地理数据库
-
-    fcs=arcpy.ListFeatureClasses(feature_dataset=ds)
-    for fc in fcs:
-        print(fc)
-        st = arcpy.Describe(fc).shapeType
-        arcpy.CreateFeatureclass_management(outpath + "\\" + outgdbname + ".gdb"+"\\"+ds,fc,st)
-
-
-
-# for workspace in workspaces:
-#     arcpy.env.workspace = workspace
-#     datasets = arcpy.ListDatasets()
-#     for ds in datasets:
-#         for fc in arcpy.ListFeatureClasses(feature_dataset=ds):
-#             path = os.path.join(arcpy.env.workspace, ds, fc)
-#             print(path)
-
-# import arcpy,os,string,sys
-# from arcpy import env
-# inpath=r"C：Users\Administrator\Desktop\mdb" #输入路径
-# outpath=r"C：Users\Administrator\Desktop\mdb2" #输出路径
-# outgdbname="cx"+".gdb" #输出数据库名称
-# dbtype="mdb" #数据库类型
-# fc="HYDA" #待合并图层
-# fc_shapetype="POLYGON"#待合并图层形状：POLYGON,POLYLINE,POINT
-# log=os.path.join(outpath,"%s's shapetype is not correct.TXT"%(fc)) #日志文件
-# arcpy.CreateFileGDB_management(outpath,outgdbname) #建立输出地理数据库
-# for root,dirs,files in os.walk(inpath): #解析输入文件夹下的文件的路径
-#     str1 = ""  # 初始化字符串
-#     for afile in files:  # 遍历文件
-#         if afile[-3:].lower() == dbtype:  # 指定脚本处理的文件类型
-#             mdb = os.path.join(root, afile)  # 获取待处理mdb 的绝对路径
-#         env.workspace = mdb  # 指定工作空间
-#         fcs = arcpy.ListFeatureClasses()  # 获取地理数据库下的图层列表
-#         if fc in fcs or fc.lower() in fcs:  # 判断指定图层是否存在
-#             fc_path = os.path.join(mdb, fc)  # 获取待处理图层的绝对路径
-#             desc = arcpy.Describe(fc_path)  # 获取指定图层属性信息
-#             if desc.shapetype.lower() == fc_shapetype.lower():  # 判断指定图层几何类型是否正确
-#                 if arcpy.Exists(outpath+os.sep+outgdbname+os.sep+fc):
-#                     pass  # 若输出数据库中已存在指定图层则pass
-#             else:
-#                 arcpy.CreateFeatureclass_management(outpath + os.sep + outgdbname, fc, fc_shapetype.upper(), fc_path)
-#             # 若输出数据库中不存在指定图层则创建图层
-#             arcpy.Append_management(fc_path, outpath + os.sep + outgdbname + os.sep + fc, "NO_TEST")  # 将输入路径下
-#             的指定图层追加至输出数据库下的同名图层
-#             if desc.shapetype.lower() != fc_shapetype.lower():
-#                 str1 = str1 + '"' + afile + '"' + ","  # 记录几何类型不正确的指定图层
-#             str = str1[:-1]
-#             opfile = open(log, "a")  # 新建并打开日志文件
-#             opfile.writelines(str)  # 将几何类型不正确的指定图层所在mdb 写入日志
-#             opfile.close()  # 关闭文本
+        return ""
