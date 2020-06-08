1f66079bfb38d1276e69f316af9e29c69fb15e87
zhaoyingchuan <18146612665@163.com>
2020-06-08 19:56:32 +0800

    修改代码

--- 29.合并多个GDB数据库.py
+++ 29.合并多个GDB数据库.py
@@ -12,7 +12,8 @@ print(gdblist)
 
 outpath="D:\\Desktop" #合并后GDB数据库所在的文件夹
 outgdbname="汇总数据库" #合并后GDB数据库的名称
-if arcpy.Exists(outpath+"\\"+outgdbname+".gdb"): #判断是否已存在将新建的GDB数据库
+gdbpath=outpath+"\\"+outgdbname+".gdb"
+if arcpy.Exists(gdbpath): #判断是否已存在将新建的GDB数据库
     pass
 else:
     arcpy.CreateFileGDB_management(outpath,outgdbname) #建立输出地理数据库
@@ -22,28 +23,28 @@ for gdb in gdblist: #循环待合并GDB数据库
     arcpy.env.workspace = gdb #将循环到的数据库作为工作空间
     datasets = arcpy.ListDatasets() #列出该数据库包含的要素数据集
     for ds in datasets: #循环要素数据集
-        if arcpy.Exists(outpath + "\\" + outgdbname + ".gdb"+"\\"+ds): #判断汇总数据库中是否已存在同名要素数据集
+        if arcpy.Exists(gdbpath+"\\"+ds): #判断汇总数据库中是否已存在同名要素数据集
             pass
         else: #如果不存在改要素数据集
             desc = arcpy.Describe(ds) #获取该要素数据集的描述
             sr = desc.spatialReference #获取该要素数据集的空间参考
-            arcpy.CreateFeatureDataset_management(outpath+"\\"+outgdbname+".gdb",ds,sr)  #在汇总数据库中创建同名的要素数据集，并且空间参考与待合并GDB数据集中的要素数据集保持一致
+            arcpy.CreateFeatureDataset_management(gdbpath,ds,sr)  #在汇总数据库中创建同名的要素数据集，并且空间参考与待合并GDB数据集中的要素数据集保持一致
             print("    "+ds) #打印该要素数据集的名称
 
         fcs=arcpy.ListFeatureClasses(feature_dataset=ds)
         for fc in fcs:
-            if arcpy.Exists(outpath + "\\" + outgdbname + ".gdb"+"\\"+ds + "\\" + fc):
+            if arcpy.Exists(gdbpath+"\\"+ds + "\\" + fc):
                 pass
             else:
-                arcpy.FeatureClassToFeatureClass_conversion(fc,outpath + "\\" + outgdbname + ".gdb"+"\\"+ds,fc)
+                arcpy.FeatureClassToFeatureClass_conversion(fc,gdbpath+"\\"+ds,fc)
                 print("        " + fc)
-                arcpy.DeleteFeatures_management(outpath + "\\" + outgdbname + ".gdb"+"\\"+ds + "\\" + fc)
+                arcpy.DeleteFeatures_management(gdbpath+"\\"+ds + "\\" + fc)
             # if arcpy.Exists(gdb+"\\"+ds+"\\"+fc):
             #     arcpy.Append_management(gdb+"\\"+ds+"\\"+fc,outpath + "\\" + outgdbname + ".gdb" + "\\" + ds + "\\" + fc, "NO_TEST")
 
 #将待合并GDB数据库中的多个同名要素类追加到汇总数据库中同名的要素类中
 print('开始合并GDB数据库中的同名要素类')
-arcpy.env.workspace = outpath+"\\"+outgdbname+".gdb"
+arcpy.env.workspace = gdbpath
 datasets = arcpy.ListDatasets()
 for ds in datasets:
     print('    '+ds)
@@ -51,6 +52,6 @@ for ds in datasets:
     for fc in fcs:
         for gdb in gdblist:
             if arcpy.Exists(gdb+"\\"+ds+"\\"+fc):
-                arcpy.Append_management(gdb+"\\"+ds+"\\"+fc, outpath + "\\" + outgdbname + ".gdb" + "\\" + ds + "\\" + fc,"NO_TEST")
+                arcpy.Append_management(gdb+"\\"+ds+"\\"+fc, gdbpath + "\\" + ds + "\\" + fc,"NO_TEST")
                 print('        '+fc+'  '+'合并成功')
 
