d05118c280493e4ef9e986f6eca013823e3f9e90
zhaoyingchuan <18146612665@163.com>
2020-10-24 16:14:50 +0800

    修改代码

--- 31.三调DLBM对应国土空间规划用途二级类.py
+++ 31.三调DLBM对应国土空间规划用途二级类.py
@@ -1,95 +1,95 @@
-def fill(Field):
-    if Field == '0304':
+def convert(DLBM):
+    if DLBM == '0101':
+        return '无'
+    elif DLBM == '0102':
+        return '无'
+    elif DLBM == '0103':
+        return '无'
+    elif DLBM == '0201':
+        return '无'
+    elif DLBM == '0202':
+        return '无'
+    elif DLBM == '0203':
+        return '无'
+    elif DLBM == '0204':
+        return '无'
+    elif DLBM == '0301':
+        return '无'
+    elif DLBM == '0302':
+        return '无'
+    elif DLBM == '0304':
         return '1702 沼泽'
-    elif Field == '0306':
+    elif DLBM == '0305':
+        return '无'
+    elif DLBM == '0306':
         return '1702 沼泽'
-    elif Field == '0402':
+    elif DLBM == '0307':
+        return '无'
+    elif DLBM == '0401':
+        return '无'
+    elif DLBM == '0402':
         return '1702 沼泽'
-    elif Field == '0603':
-        return '1602 盐田'
-    elif Field == '1106':
-        return '1703 滩涂'
-    elif Field == '1108':
-        return '1702 沼泽'
-    elif Field == '0101':
+    elif DLBM == '0403':
         return '无'
-    elif Field == '0102':
-        return '无'
-    elif Field == '0103':
-        return '无'
-    elif Field == '0201':
-        return '无'
-    elif Field == '0202':
-        return '无'
-    elif Field == '0203':
-        return '无'
-    elif Field == '0204':
-        return '无'
-    elif Field == '0301':
-        return '无'
-    elif Field == '0302':
-        return '无'
-    elif Field == '0305':
-        return '无'
-    elif Field == '0307':
-        return '无'
-    elif Field == '0401':
-        return '无'
-    elif Field == '0403':
-        return '无'
-    elif Field == '0404':
+    elif DLBM == '0404':
         return '1805 其他草地'
-    elif Field == '0602':
+    elif DLBM == '0602':
         return '1601 采矿用地'
-    elif Field == '0701':
+    elif DLBM == '0603':
+        return '1602 盐田'
+    elif DLBM == '0701':
         return '0601 城镇住宅用地'
-    elif Field == '0702':
+    elif DLBM == '0702':
         return '0602 农村住宅用地'
-    elif Field == '08H1':
+    elif DLBM == '08H1':
         return '0701 行政办公用地'
-    elif Field == '0810A':
+    elif DLBM == '0810A':
         return '1203 广场用地'
-    elif Field == '1001':
+    elif DLBM == '1001':
         return '1401 铁路用地'
-    elif Field == '1002':
+    elif DLBM == '1002':
         return '1003 城市轨道交通用地'
-    elif Field == '1003':
+    elif DLBM == '1003':
         return '1402 公路用地'
-    elif Field == '1006':
+    elif DLBM == '1006':
         return '0502 农村道路'
-    elif Field == '1007':
+    elif DLBM == '1007':
         return '1402 机场用地'
-    elif Field == '1008':
+    elif DLBM == '1008':
         return '1403 港口码头用地'
-    elif Field == '1009':
+    elif DLBM == '1009':
         return '1405 管道运输用地'
-    elif Field == '1101':
+    elif DLBM == '1101':
         return '1901 河流水面'
-    elif Field == '1102':
+    elif DLBM == '1102':
         return '1902 湖泊水面'
-    elif Field == '1103':
+    elif DLBM == '1103':
         return '1903 水库水面'
-    elif Field == '1104':
+    elif DLBM == '1104':
         return '0504 坑塘水面'
-    elif Field == '1104A':
+    elif DLBM == '1104A':
         return '0504 坑塘水面'
-    elif Field == '1107':
+    elif DLBM == '1106':
+        return '1703 滩涂'
+    elif DLBM == '1107':
         return '0505 沟渠'
-    elif Field == '1109':
+    elif DLBM == '1108':
+        return '1702 沼泽'
+    elif DLBM == '1109':
         return '1406 区域公用设施用地'
-    elif Field == '1201':
+    elif DLBM == '1201':
         return '无'
-    elif Field == '1202':
+    elif DLBM == '1202':
         return '0501 设施农用地'
-    elif Field == '1203':
+    elif DLBM == '1203':
         return '0503 田坎'
-    elif Field == '1204':
+    elif DLBM == '1204':
         return '1801 盐碱地'
-    elif Field == '1205':
+    elif DLBM == '1205':
         return '1802 沙地'
-    elif Field == '1206':
+    elif DLBM == '1206':
         return '1803 裸土地'
-    elif Field == '1207':
+    elif DLBM == '1207':
         return '1804 裸岩石砾地'
     else:
         return ""
