05deb9ac5879ea383cbc97b175e28febb823ce75
zhaoyingchuan <18146612665@163.com>
2020-10-24 16:06:25 +0800

    修改代码

--- 30.三调DLBM对应国土空间规划用途一级类.py
+++ 30.三调DLBM对应国土空间规划用途一级类.py
@@ -1,107 +1,107 @@
-def fill(Field):
-    if Field == '0304':
-        return '17 湿地'
-    elif Field == '0306':
-        return '17 湿地'
-    elif Field == '0402':
-        return '17 湿地'
-    elif Field == '0603':
-        return '16 采矿盐田用地'
-    elif Field == '1106':
-        return '17 湿地'
-    elif Field == '1108':
-        return '17 湿地'
-    elif Field == '0101':
+def convert(DLBM):
+    if DLBM == '0101':
         return '01 耕地'
-    elif Field == '0102':
+    elif DLBM == '0102':
         return '01 耕地'
-    elif Field == '0103':
+    elif DLBM == '0103':
         return '01 耕地'
-    elif Field == '0201':
+    elif DLBM == '0201':
         return '02 园地'
-    elif Field == '0202':
+    elif DLBM == '0202':
         return '02 园地'
-    elif Field == '0203':
+    elif DLBM == '0203':
         return '02 园地'
-    elif Field == '0204':
+    elif DLBM == '0204':
         return '02 园地'
-    elif Field == '0301':
+    elif DLBM == '0301':
         return '03 林地'
-    elif Field == '0302':
+    elif DLBM == '0302':
         return '03 林地'
-    elif Field == '0305':
+    elif DLBM == '0304':
+        return '17 湿地'
+    elif DLBM == '0305':
         return '03 林地'
-    elif Field == '0307':
+    elif DLBM == '0306':
+        return '17 湿地'
+    elif DLBM == '0307':
         return '03 林地'
-    elif Field == '0401':
+    elif DLBM == '0401':
         return '04 牧草地'
-    elif Field == '0403':
+    elif DLBM == '0402':
+        return '17 湿地'
+    elif DLBM == '0403':
         return '04牧草地'
-    elif Field == '0404':
+    elif DLBM == '0404':
         return '18 其他自然保留地'
-    elif Field == '0508':
+    elif DLBM == '0508':
         return '09 仓储用地'
-    elif Field == '0601':
+    elif DLBM == '0601':
         return '08 工业用地'
-    elif Field == '0602':
+    elif DLBM == '0602':
         return '16 采矿盐田用地'
-    elif Field == '0701':
+    elif DLBM == '0603':
+        return '16 采矿盐田用地'
+    elif DLBM == '0701':
         return '06 居住用地'
-    elif Field == '0702':
+    elif DLBM == '0702':
         return '06 居住用地'
-    elif Field == '08H1':
+    elif DLBM == '08H1':
         return '07 公共设施用地'
-    elif Field == '0810':
+    elif DLBM == '0810':
         return '12 绿地与广场用地'
-    elif Field == '0810A':
+    elif DLBM == '0810A':
         return '12 绿地与广场用地'
-    elif Field == '09':
+    elif DLBM == '09':
         return '15 特殊用地'
-    elif Field == '1001':
+    elif DLBM == '1001':
         return '14 区域基础设施用地'
-    elif Field == '1002':
+    elif DLBM == '1002':
         return '10 道路与交通设施用地'
-    elif Field == '1003':
+    elif DLBM == '1003':
         return '14 区域基础设施用地'
-    elif Field == '1004':
+    elif DLBM == '1004':
         return '10 道路与交通设施用地'
-    elif Field == '1005':
+    elif DLBM == '1005':
         return '10 道路与交通设施用地'
-    elif Field == '1006':
+    elif DLBM == '1006':
         return '05 其他农用地'
-    elif Field == '1007':
+    elif DLBM == '1007':
         return '14 区域基础设施用地'
-    elif Field == '1008':
+    elif DLBM == '1008':
         return '14 区域基础设施用地'
-    elif Field == '1009':
+    elif DLBM == '1009':
         return '14 区域基础设施用地'
-    elif Field == '1101':
+    elif DLBM == '1101':
         return '19 陆地水域'
-    elif Field == '1102':
+    elif DLBM == '1102':
         return '19 陆地水域'
-    elif Field == '1103':
+    elif DLBM == '1103':
         return '19 陆地水域'
-    elif Field == '1104':
+    elif DLBM == '1104':
         return '05 其他农用地'
-    elif Field == '1104A':
+    elif DLBM == '1104A':
         return '05 其他农用地'
-    elif Field == '1107':
+    elif DLBM == '1106':
+        return '17 湿地'
+    elif DLBM == '1107':
         return '05 其他农用地'
-    elif Field == '1109':
+    elif DLBM == '1108':
+        return '17 湿地'
+    elif DLBM == '1109':
         return '14 区域基础设施用地'
-    elif Field == '1201':
+    elif DLBM == '1201':
         return '13 留白用地'
-    elif Field == '1202':
+    elif DLBM == '1202':
         return '05 其他农用地'
-    elif Field == '1203':
+    elif DLBM == '1203':
         return '05 其他农用地'
-    elif Field == '1204':
+    elif DLBM == '1204':
         return '18 其他自然保留地'
-    elif Field == '1205':
+    elif DLBM == '1205':
         return '18 其他自然保留地'
-    elif Field == '1206':
+    elif DLBM == '1206':
         return '18 其他自然保留地'
-    elif Field == '1207':
+    elif DLBM == '1207':
         return '18 其他自然保留地'
     else:
         return ""
